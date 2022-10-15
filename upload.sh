c8de59b8e145f17532d181b1db959cc3b2f510ba
uretgec <iletisim@uretgec.com>
2022-10-15 19:54:52 +0300

    fix: SSH_PRIVATE_KEY arg add upload script

--- upload.sh
+++ upload.sh
@@ -4,8 +4,11 @@
 WHEREIAM=$(pwd)
 IP_ADDR=$(cat $WHEREIAM/IP_ADDR)
 
+ARGS=("$@")
+SSH_PRIVATE_KEY="${ARGS[0]}"
+
 echo "Build go files"
 cd dmcli && go run . build go --config .build.prod.yaml
 
 echo "Rsync file to server"
-rsync -av --progress -e "ssh -i ~/.ssh/gitdm_hetzner" --exclude={'*.dev.env','*.dev.yaml'} $WHEREIAM/build/prod/* root@$IP_ADDR:/usr/local/bin
+rsync -av --progress -e "ssh -i ~/$SSH_PRIVATE_KEY" --exclude={'*.dev.env','*.dev.yaml'} $WHEREIAM/build/prod/* root@$IP_ADDR:/usr/local/bin
