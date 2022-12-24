9ecc0e44cbcc2d6c78435451ee35732871d6e706
uretgec <iletisim@uretgec.com>
2022-12-24 22:28:09 +0300

    fix: update template path of 404 and 502 pages

--- dmweb/middlewares/notFound.go
+++ dmweb/middlewares/notFound.go
@@ -6,7 +6,7 @@ import (
 
 func NotFound() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		return c.Status(fiber.StatusNotFound).Render("404", fiber.Map{
+		return c.Status(fiber.StatusNotFound).Render("templates/404", fiber.Map{
 			"Title":   "404 - Not Found",
 			"Message": "Oopps nothing is here!",
 		}, "templates/layouts/error") // HTTP:404
